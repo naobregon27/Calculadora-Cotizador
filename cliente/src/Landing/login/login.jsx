6f020257f423f2d1cb70352c0e526ce7a712292b
naobregon27 <na.obregon27@hotmail.com>
2023-12-28 17:39:07 -0300

    nuevos cambios

--- cliente/src/Landing/login/login.jsx
+++ cliente/src/Landing/login/login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     const [password, setPassword] = useState('');
 
     const irAUrl = () => {
-        window.location.href = 'http://localhost:5000/';
+        window.location.href = 'https://calculadora-cortizador.onrender.com/';
     };
 
     const handleSubmit = (event) => {
@@ -18,7 +18,7 @@ const Login = () => {
 
         if (username === correctUsername && password === correctPassword) {
             alert('Inicio de sesión exitoso');
-            window.location.href = 'http://localhost:5000/';
+            window.location.href = 'https://calculadora-cortizador.onrender.com/';
         } else {
             alert('Nombre de usuario o contraseña incorrectos');
         }
